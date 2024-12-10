6deed7c33dcc942140c2b98d81a4a1e2e67233b0
Aleksey Perestoronin <Aleksey.Perestoronin@myoffice.team>
2024-12-10 14:45:51 +0600

    sort colors

--- Cpp/src/OpenSources/LetCode/support.h
+++ Cpp/src/OpenSources/LetCode/support.h
@@ -2,6 +2,15 @@
 #include <gtest/gtest.h>
 
 namespace Support {
+template <class Array>
+void compareTwoArray(const Array& expected_array, const Array& real_array) {
+    ASSERT_EQ(expected_array.size(), real_array.size());
+    size_t index{0};
+    for (auto it1 = expected_array.begin(), it2 = real_array.begin(); it1 != expected_array.end(); ++it1, ++it2, ++index) {
+        ASSERT_EQ(*it1, *it2) << "index = " << index;
+    }
+}
+
 template <class Matrix>
 void compareTwoMatrix(const Matrix& expected_matrix, const Matrix& real_matrix) {
     ASSERT_EQ(expected_matrix.size(), real_matrix.size());
